497d6b0a1c974e87654d38e7f083d9c234d8ad9e
Cosmo Matias Gomes <ducosmo@MacBook-Pro-de-Cosmo.local>
2026-08-28 01:05:47 -0300

    Configuração do EAS Build para APK

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,18 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
 
+/** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
+
+config.server = {
+  ...config.server,
+  enhanceMiddleware: (middleware, server) => {
+    return (req, res, next) => {
+      res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+      res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+      return middleware(req, res, next);
+    };
+  },
+};
 
 module.exports = withNativeWind(config, { input: "./global.css" });
